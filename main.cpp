d9bd2317d38a41d3909a9e516e632df45b6e1fb6
= <lucas7forato@gmail.com>
2023-09-25 11:17:15 -0300

    feat: added first programm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
+
+// this tells the compiler that I am using this namespace, this will make it possible to write shorter code for I do not have to write std::cout
+using namespace std;
+
+string get_instructions() {
+    string instructions = "first, you get up, then you go upstairs, then you open the door on the left, and then you enter";
+    return instructions;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string instructions = get_instructions();
+    cout << instructions << endl;
     return 0;
 }
+
+// comments
+// We can chain << operators together to print out multiple things
