b38328e15283aa80ae0cd7a849f77327150c8406
Lucas Forato <lforato@soap.health>
2024-01-24 13:38:54 -0300

    feat: changed tic_tac_toe

---
+++ tasks/task1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  int n = 0;
+  cout << "Enter a number: "; 
+  cin >> n;
+
+  cout << "Two times " << n << " is " << 2 * n << '\n';
+}
---
+++ variable_init/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int x;
+
+  cout << "x = " << x << '\n';
+
+  return 0;
+}
