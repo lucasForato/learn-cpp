fd48ba501975babb0bfcb812553edd7c9cce347e
Lucas Forato <lforato@soap.health>
2024-01-27 18:53:07 -0300

    feat: changed stuff

---
+++ auth/CMakeFiles/auth_api.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/auth_api.dir/src/main.cpp.o"
+  "CMakeFiles/auth_api.dir/src/main.cpp.o.d"
+  "auth_api"
+  "auth_api.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/auth_api.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ auth/CMakeFiles/auth_api.dir/src/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/auth_api.dir/src/main.cpp.o: \
+  /Users/forato/Projects/personal/learn-cpp/auth/src/main.cpp
