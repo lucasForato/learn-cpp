9aa682685ad9d88328436dd8173d8ba9d3f1b25a
Lucas Forato <lforato@soap.health>
2024-02-05 19:04:47 -0300

    feat: added clang format file

--- auth/src/main.cpp
+++ auth/src/main.cpp
@@ -3,51 +3,51 @@
 #include "db/QueryResponse.h"
 #include <crow/app.h>
 
-int main() {
-  crow::SimpleApp app;
+int main()
+{
+	crow::SimpleApp app;
 
-  CROW_ROUTE(app, "/health").methods("GET"_method)([]() {
-    crow::json::wvalue response({{"status", "ok"}});
-    return response;
-  });
+	CROW_ROUTE(app, "/health").methods("GET"_method)([]() {
+		crow::json::wvalue response({{"status", "ok"}});
+		return response;
+	});
 
-  CROW_ROUTE(app, "/signup")
-      .methods("POST"_method)([](const crow::request &req) {
-        crow::json::rvalue body = crow::json::load(req.body);
+	CROW_ROUTE(app, "/signup").methods("POST"_method)([](const crow::request& req) {
+		crow::json::rvalue body = crow::json::load(req.body);
 
-        // validation
-        if (!body.has("username") || !body.has("email") ||
-            !body.has("password")) {
-          return crow::response(400);
-        }
+		// validation
+		if(!body.has("username") || !body.has("email") || !body.has("password"))
+		{
+			return crow::response(400);
+		}
 
-        string password = body["password"].s();
-        string hash = BCrypt::generateHash(password);
+		string password = body["password"].s();
+		string hash = BCrypt::generateHash(password);
 
-        Database db("cpp.db");
-        string sql = "INSERT INTO users (username, email, password)"
-                     "VALUES (?, ?, ?);";
+		Database db("cpp.db");
+		string sql = "INSERT INTO users (username, email, password)"
+					 "VALUES (?, ?, ?);";
 
-        vector<string> values = {body["username"].s(), body["email"].s(), hash};
+		vector<string> values = {body["username"].s(), body["email"].s(), hash};
 
-        db.exec(sql, values);
+		db.exec(sql, values);
 
-        crow::json::wvalue response({{"status", 200}});
+		crow::json::wvalue response({{"status", 200}});
 
-        return crow::response(response);
-      });
+		return crow::response(response);
+	});
 
-  CROW_ROUTE(app, "/users").methods("GET"_method)([]() {
-    Database db("cpp.db");
+	CROW_ROUTE(app, "/users").methods("GET"_method)([]() {
+		Database db("cpp.db");
 
-    string sql = "SELECT * FROM users;";
-    QueryResponse<User> res = db.exec(sql);
+		string sql = "SELECT * FROM users;";
+		QueryResponse<User> res = db.exec(sql);
 
-    vector<crow::json::wvalue> users = res.dto();
+		vector<crow::json::wvalue> users = res.dto();
 
-    crow::json::wvalue response({{"status", 200}, {"users", users}});
-    return response;
-  });
+		crow::json::wvalue response({{"status", 200}, {"users", users}});
+		return response;
+	});
 
-  app.port(8000).multithreaded().run();
+	app.port(8000).multithreaded().run();
 }
