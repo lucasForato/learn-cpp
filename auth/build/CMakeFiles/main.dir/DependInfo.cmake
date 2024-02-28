16e67c3b5ac1897c53facb76ce57664f82316d25
Lucas Forato <lforato@soap.health>
2024-02-28 14:34:03 -0300

    fix: changed a lot of things

---
+++ auth/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/forato/Projects/personal/learn-cpp/auth/src/db/Database.cpp" "CMakeFiles/main.dir/src/db/Database.cpp.o" "gcc" "CMakeFiles/main.dir/src/db/Database.cpp.o.d"
+  "/Users/forato/Projects/personal/learn-cpp/auth/src/db/QueryResponse.cpp" "CMakeFiles/main.dir/src/db/QueryResponse.cpp.o" "gcc" "CMakeFiles/main.dir/src/db/QueryResponse.cpp.o.d"
+  "/Users/forato/Projects/personal/learn-cpp/auth/src/db/models/User.cpp" "CMakeFiles/main.dir/src/db/models/User.cpp.o" "gcc" "CMakeFiles/main.dir/src/db/models/User.cpp.o.d"
+  "/Users/forato/Projects/personal/learn-cpp/auth/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/Users/forato/Projects/personal/learn-cpp/auth/src/shared/Result.cpp" "CMakeFiles/main.dir/src/shared/Result.cpp.o" "gcc" "CMakeFiles/main.dir/src/shared/Result.cpp.o.d"
+  "/Users/forato/Projects/personal/learn-cpp/auth/src/usecases/ListUsersUseCase.cpp" "CMakeFiles/main.dir/src/usecases/ListUsersUseCase.cpp.o" "gcc" "CMakeFiles/main.dir/src/usecases/ListUsersUseCase.cpp.o.d"
+  "/Users/forato/Projects/personal/learn-cpp/auth/src/usecases/SigninUseCase.cpp" "CMakeFiles/main.dir/src/usecases/SigninUseCase.cpp.o" "gcc" "CMakeFiles/main.dir/src/usecases/SigninUseCase.cpp.o.d"
+  "/Users/forato/Projects/personal/learn-cpp/auth/src/usecases/SignupUseCase.cpp" "CMakeFiles/main.dir/src/usecases/SignupUseCase.cpp.o" "gcc" "CMakeFiles/main.dir/src/usecases/SignupUseCase.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ auth/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/db/Database.cpp.o"
+  "CMakeFiles/main.dir/src/db/Database.cpp.o.d"
+  "CMakeFiles/main.dir/src/db/QueryResponse.cpp.o"
+  "CMakeFiles/main.dir/src/db/QueryResponse.cpp.o.d"
+  "CMakeFiles/main.dir/src/db/models/User.cpp.o"
+  "CMakeFiles/main.dir/src/db/models/User.cpp.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/shared/Result.cpp.o"
+  "CMakeFiles/main.dir/src/shared/Result.cpp.o.d"
+  "CMakeFiles/main.dir/src/usecases/ListUsersUseCase.cpp.o"
+  "CMakeFiles/main.dir/src/usecases/ListUsersUseCase.cpp.o.d"
+  "CMakeFiles/main.dir/src/usecases/SigninUseCase.cpp.o"
+  "CMakeFiles/main.dir/src/usecases/SigninUseCase.cpp.o.d"
+  "CMakeFiles/main.dir/src/usecases/SignupUseCase.cpp.o"
+  "CMakeFiles/main.dir/src/usecases/SignupUseCase.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
